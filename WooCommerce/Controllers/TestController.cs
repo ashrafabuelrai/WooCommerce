6ad9b04f6b43771a6e8c6a873af3bc7e69332a98
Ashraf Abu Elrai <126774761+ashrafabuelrai@users.noreply.github.com>
2026-07-30 17:49:46 +0300

    Update

---
+++ WooCommerce/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WooCommerce.Application.Services.Interfaces;
+
+namespace WooCommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IZohoAuthService _authService;
+
+        public TestController(IZohoAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet("token")]
+        public async Task<IActionResult> GetToken()
+        {
+            var token = await _authService.GetAccessTokenAsync();
+
+            return Ok(token);
+        }
+    }
+}
